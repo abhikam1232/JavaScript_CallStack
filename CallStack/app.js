a2adb8514a60bcaf474d11b6e4d758036958eea3
Abhishek <abhikam152002@gmail.com>
2025-12-25 15:13:43 +0530

    callstack

--- CallStack/app.js
+++ CallStack/app.js
@@ -1,5 +1,12 @@
 function work(){
     console.log(" Today's tasks have started executing!!! ");
+    console.log(" This is the work function !! ");
 }
 
-work(); // This is a function call 
+function fun() {
+    console.log(" This is fun function call work ");
+    work();
+}
+
+console.log(" This function is calling fun function");
+fun();
