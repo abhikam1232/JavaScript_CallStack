b896a4f531898af80fe9b4f793b0a6db94a164ec
Abhishek <abhikam152002@gmail.com>
2025-12-25 15:39:47 +0530

    second callsatck

--- CallStack/app.js
+++ CallStack/app.js
@@ -4,9 +4,31 @@ function work(){
 }
 
 function fun() {
-    console.log(" This is fun function call work ");
+    console.log(" This is fun function calls work ");
     work();
 }
 
-console.log(" This function is calling fun function");
+console.log(" Second function implemented ");
 fun();
+console.log(" Functions Implemented");
+
+
+// Second Assignmnet Ques:- 
+function first() {
+    console.log(" Inside first() ");
+    second();
+}
+
+function second() {
+    console.log("Inside second() ");
+    third();
+}
+
+function third() {
+    console.log(" Inside third() ");
+}
+
+
+console.log(" Program has been started");
+first();
+console.log("program Ended !!");
