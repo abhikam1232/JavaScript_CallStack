04c439ad630082d0154f42c7815d26ddee65ccdd
Abhishek <abhikam152002@gmail.com>
2025-12-31 13:16:57 +0530

    breakpoints

--- CallStack/app.js
+++ CallStack/app.js
@@ -1,4 +1,4 @@
-/** function work(){
+ function work(){
     console.log(" Today's tasks have started executing!!! ");
     console.log(" This is the work function !! ");
 }
@@ -12,7 +12,7 @@ console.log(" Second function implemented ");
 fun();
 console.log(" Functions Implemented");
 
-
+/** 
 // Second Assignmnet Ques:- 
 function first() {
     console.log(" Inside first() ");
@@ -31,13 +31,13 @@ function third() {
 
 console.log(" Program has been started");
 first();
-console.log("program Ended !!"); **/
+console.log("program Ended !!");
 
 
 // Third call stack implemented:- 
 
 
-function one() {
+/** function one() {
     return 1; // This function is just returning one value which is "1"
 }
 
@@ -50,4 +50,21 @@ function three() {
     console.log(ans);
 }
 
-three();
+three(); **/
+
+// Javascript call stack using breakpoints:- 
+
+function ten() {
+    return 10;
+}
+
+function twenty() {
+    return ten() + ten();
+}
+
+function thirty() {
+    let answer = twenty() + ten();
+    console.log(answer);
+}
+
+thirty();
