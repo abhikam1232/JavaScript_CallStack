eecce01378bbfaa4d2139ccf1509d34950b6aa17
Abhishek <abhikam152002@gmail.com>
2026-01-05 10:54:45 +0530

    callbackhells

--- CallbackHell/app.js
+++ CallbackHell/app.js
@@ -2,7 +2,7 @@
 
 let h1 = document.querySelector("h1");
 
-function changeColor(color, delay, nextColor) {
+/** function changeColor(color, delay, nextColor) {
     setTimeout(() => {
         h1.style.color = color;
         if (nextColor) nextColor();
@@ -20,4 +20,44 @@ changeColor("red", 1000, () => {
         });
     });
 
+}); **/
+
+
+let h2 = document.querySelector("h2");
+
+function changeAppear(colour, delay, nextColour) {
+    setTimeout(() => {
+        h2.style.color = colour;
+        h2.style.fontSize = "2.5em";
+        if (nextColour) nextColour();
+    }, delay);
+
+
+};
+
+changeAppear("blue", 1500, () => {
+    changeAppear("maroon", 1500, () => {
+        changeAppear("purple", 1500, () => {
+            changeAppear("green", 1500);
+
+        });
+    });
 });
+
+// js function to implement callback hells for animate:- 
+
+function animateText(step, back,  delaay, nextStep){
+    setTimeout( () => {
+        h1.style.color = step;
+        h1.style.backgroundColor = back;
+        if (nextStep) nextStep();
+    }, delaay);
+};
+
+animateText("lightblue", "blue", 1000, ()=> {
+    animateText("maroon", "white", 1000, ()=> {
+        animateText("black", "grey", 1000, ()=> {
+            animateText("pink", "red", 1000);
+        });
+    });
+})
