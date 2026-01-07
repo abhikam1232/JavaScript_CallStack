8744ff54d500efde03832685109a6af04ae95ded
Abhishek <abhikam152002@gmail.com>
2026-01-07 16:02:42 +0530

    promises revise

--- Promises/app.js
+++ Promises/app.js
@@ -155,3 +155,43 @@ uploadFile("File is loading")
     .then(() => {
         console.info(" File has not yet Loaded Successfully");
     });
+
+// Login user:- 
+
+function loginUser(user, pass) {
+    return new Promise((resolve, reject) => {
+        if (user == "admin" && pass == "1234") {
+            resolve;
+        } else {
+            reject();
+        }
+    });
+
+};
+
+loginUser("admin", "1234")
+    .catch(() => {
+        console.info(" User has sucessfully Logged In the Page");
+    })
+    .then(() => {
+        console.info(" User Failed Login!! ");
+    });
+
+
+/** function loginUser(user, pass) {
+    return new Promise((resolve, reject) => {
+        if (user === "admin" && pass === "1234") {
+            resolve();
+        } else {
+            reject();
+        }
+    });
+}
+
+loginUser("admin", "1234")
+    .then(() => {
+        console.info("User has successfully logged in ✅");
+    })
+    .catch(() => {
+        console.info("User failed to login ❌");
+    }); **/
